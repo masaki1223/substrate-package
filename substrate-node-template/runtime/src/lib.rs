79efeac052f7c876bbc68a536b5842cc62b6d730
Shawn Tabrizi <shawntabrizi@gmail.com>
2019-02-26 15:45:02 +0100

    add demo to latest

--- substrate-node-template/runtime/src/lib.rs
+++ substrate-node-template/runtime/src/lib.rs
@@ -46,8 +46,7 @@ pub type BlockNumber = u64;
 /// Index of an account's extrinsic in the chain.
 pub type Nonce = u64;
 
-/// Used for the module template in `./template.rs`
-mod template;
+mod demo;
 
 /// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
 /// the specifics of the runtime. They can then be made to be agnostic over specific formats
@@ -175,10 +174,7 @@ impl sudo::Trait for Runtime {
 	type Proposal = Call;
 }
 
-/// Used for the module template in `./template.rs`
-impl template::Trait for Runtime { 
-	type Event = Event;
-}
+impl demo::Trait for Runtime {}
 
 construct_runtime!(
 	pub enum Runtime with Log(InternalLog: DigestItem<Hash, Ed25519AuthorityId>) where
@@ -194,8 +190,7 @@ construct_runtime!(
 		Balances: balances,
 		Sudo: sudo,
 		Fees: fees::{Module, Storage, Config<T>, Event<T>},
-		// Used for the module template in `./template.rs`
-		TemplateModule: template::{Module, Call, Storage, Event<T>},
+		Demo: demo::{Module, Call, Storage},
 	}
 );
 
